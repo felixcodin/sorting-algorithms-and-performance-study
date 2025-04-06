6800bdf7482f42fe2c474884661df6e1ea84bffe
Nguyen Nguyen <brightsmileslikesunshine@gmail.com>
2025-04-06 22:04:06 +0700

    Update data_generator.cpp file

--- src/utils/data_generator.cpp
+++ src/utils/data_generator.cpp
@@ -1,19 +1,26 @@
 #include "../include/utils/data_generator.hpp"
 
+std::random_device rd;
+std::mt19937 gen(rd());
+
 template <typename T>
 std::vector<T> generateRandomVector(int size, int min, int max) 
 {
     std::vector<T> vec(size);
-    std::random_device rd;
-    std::mt19937 gen(rd());
     std::uniform_int_distribution<> distrib(min, max);
   
-    for (T i = 0; i < size; i++) 
+    for (int i = 0; i < size; i++) 
     {
         vec[i] = distrib(gen);
     }
   
     return vec;
+}
+
+int generateRandomNumber(int min, int max)
+{
+    std::uniform_int_distribution<> distribution(min, max);
+    return distribution(gen);
 }
 
 template std::vector<int> generateRandomVector<int>(int, int, int);
