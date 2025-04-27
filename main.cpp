4f672c0615cf74cd3bcff6f9d33e7f013d77093e
Nguyen Nguyen <brightsmileslikesunshine@gmail.com>
2025-04-27 12:47:53 +0700

    Update test cases

--- main.cpp
+++ main.cpp
@@ -30,69 +30,152 @@ int main()
 
     int maxElement = k;
     int minElement = 0;
-    int sizeOfGroupOne = n;
-    int sizeOfGroupTwoAndThree = n*10*10;
+    for (int i = 1; i <= 1; i += 2)
+    {
+        int sizeOfGroupOne = n*i;
+        int sizeOfGroupTwoAndThree = n*i*10*10;
 
-    vector<int> originalVectorGroupOne = generateRandomVector<int>(sizeOfGroupOne, minElement, maxElement);
-    vector<int> originalVectorGroupTwoAndThree = generateRandomVector<int>(sizeOfGroupTwoAndThree, minElement, maxElement);
-    vector<int> originalSortedVectorGroupOne = generateAlreadySortedVectorGroupOne<int>(sizeOfGroupOne, minElement, maxElement);
-    vector<int> originalSortedVectorGroupOne = generateAlreadySortedVectorGroupTwoAndThree<int>(sizeOfGroupTwoAndThree, minElement, maxElement);
+        vector<int> originalVectorGroupOne = generateRandomVector<int>(sizeOfGroupOne, minElement, maxElement);
+        vector<int> originalVectorGroupTwoAndThree = generateRandomVector<int>(sizeOfGroupTwoAndThree, minElement, maxElement);
+        vector<int> originalSortedVectorGroupOne = generateAlreadySortedVector<int>(sizeOfGroupOne, minElement, maxElement);
+        vector<int> originalSortedVectorGroupTwoAndThree = generateAlreadySortedVector<int>(sizeOfGroupTwoAndThree, minElement, maxElement);
+        vector<int> originalReverseSortedVectorGroupOne = generateReverseSortedVector<int>(sizeOfGroupOne, minElement, maxElement);
+        vector<int> originalReverseSortedVectorGroupTwoAndThree = generateReverseSortedVector<int>(sizeOfGroupTwoAndThree, minElement, maxElement);
+        vector<int> originalNearlySortedVectorGroupOne = generateNearlySortedVector<int>(sizeOfGroupOne, minElement, maxElement);
+        vector<int> originalNearlySortedVectorGroupTwoAndThree = generateNearlySortedVector<int>(sizeOfGroupTwoAndThree, minElement, maxElement);
 
-    cout << "*********Group 1********" << endl;
-    
-    vector<pair<string, function<void(vector<int>&)>>> algorithmsGroupOne = {
-        {"Selection Sort", selectionSort<int>},
-        {"Insertion Sort", insertionSort<int>},
-        {"BinaryInsertion Sort", binaryInsertionSort<int>},
-        {"Bubble Sort", bubbleSort<int>},
-        {"Shaker Sort", shakerSort<int>},
-        {"Shell Sort", shellSort<int>}
-    };
+        vector<pair<string, function<void(vector<int>&)>>> algorithmsGroupOne = {
+            {"Selection Sort", selectionSort<int>},
+            {"Insertion Sort", insertionSort<int>},
+            {"BinaryInsertion Sort", binaryInsertionSort<int>},
+            {"Bubble Sort", bubbleSort<int>},
+            {"Shaker Sort", shakerSort<int>},
+            {"Shell Sort", shellSort<int>},
+            {"Heap Sort", heapSort<int>},
+            {"Merge Sort", mergeSort<int>},
+            {"Natural Merge Sort", naturalMergeSort<int>},
+            {"Quick Sort", quickSort<int>},
+            {"std::sort", stdSort<int>},
+            {"Radix Sort", radixSort<int>},
+            {"Counting Sort", countingSort<int>}
+        };
 
-    cout << "*********Group 2*********" << endl;
-    vector<pair<string, function<void(vector<int>&)>>> algorithmsGroupTwo = {
-        {"Heap Sort", heapSort<int>},
-        {"Merge Sort", mergeSort<int>},
-        {"Natural Merge Sort", naturalMergeSort<int>},
-        {"Quick Sort", quickSort<int>},
-        {"std::sort", stdSort<int>}
-    };
+        vector<pair<string, function<void(vector<int>&)>>> algorithmsGroupTwo = {
+            {"Heap Sort", heapSort<int>},
+            {"Merge Sort", mergeSort<int>},
+            {"Natural Merge Sort", naturalMergeSort<int>},
+            {"Quick Sort", quickSort<int>},
+            {"std::sort", stdSort<int>},
+            {"Shell Sort", shellSort<int>},
+            {"Radix Sort", radixSort<int>},
+            {"Counting Sort", countingSort<int>}
+        };
 
-    cout << "*********Group 3*********" << endl;
-    vector<pair<string, function<void(vector<int>&)>>> algorithmsGroupThree = {
-        {"Radix Sort", radixSort<int>},
-        {"Counting Sort", countingSort<int>}
-    };
+        vector<pair<string, function<void(vector<int>&)>>> algorithmsGroupOneWithAlreadySortedVector = {
+            {"Selection Sort", selectionSort<int>},
+            {"Insertion Sort", insertionSort<int>},
+            {"Binary Insertion Sort", binaryInsertionSort<int>},
+            {"Bubble Sort", bubbleSort<int>},
+            {"Shaker Sort", shakerSort<int>},
+            {"Shell Sort", shellSort<int>},
+            {"Heap Sort", heapSort<int>},
+            {"Merge Sort", mergeSort<int>},
+            {"Natural Merge Sort", naturalMergeSort<int>},
+            {"Quick Sort", quickSort<int>},
+            {"std::sort", stdSort<int>},
+            {"Radix Sort", radixSort<int>},
+            {"Counting Sort", countingSort<int>}
+        };
 
-    vector<pair<string, function<void(vector<int>&)>>> algorithmsGroupOneWithAlreadySortedVector = {
-        {"Selection Sort", selectionSort<int>},
-        {"Insertion Sort", insertionSort<int>},
-        {"Binary Insertion Sort", binaryInsertionSort<int>},
-        {"Bubble Sort", bubbleSort<int>},
-        {"Shaker Sort", shakerSort<int>},
-        {"Shell Sort", shellSort<int>}
-    };
+        vector<pair<string, function<void(vector<int>&)>>> algorithmsGroupTwoWithAlreadySortedVector = {
+            {"Heap Sort", heapSort<int>},
+            {"Merge Sort", mergeSort<int>},
+            {"Natural Merge Sort", naturalMergeSort<int>},
+            {"Quick Sort", quickSort<int>},
+            {"std::sort", stdSort<int>},
+            {"Shell Sort", shellSort<int>},
+            {"Radix Sort", radixSort<int>},
+            {"Counting Sort", countingSort<int>}
+        };
 
-    vector<pair<string, function<void(vector<int>&)>>> algorithmsGroupTwoWithAlreadySortedVector = {
-        {"Heap Sort", heapSort<int>},
-        {"Merge Sort", mergeSort<int>},
-        {"Natural Merge Sort", naturalMergeSort<int>},
-        {"Quick Sort", quickSort<int>},
-        {"std::sort", stdSort<int>}
-    };
+        vector<pair<string, function<void(vector<int>&)>>> algorithmsGroupOneWithReverseSortedVector = {
+            {"Selection Sort", selectionSort<int>},
+            {"Insertion Sort", insertionSort<int>},
+            {"Binary Insertion Sort", binaryInsertionSort<int>},
+            {"Bubble Sort", bubbleSort<int>},
+            {"Shaker Sort", shakerSort<int>},
+            {"Shell Sort", shellSort<int>},
+            {"Heap Sort", heapSort<int>},
+            {"Merge Sort", mergeSort<int>},
+            {"Natural Merge Sort", naturalMergeSort<int>},
+            {"Quick Sort", quickSort<int>},
+            {"std::sort", stdSort<int>},
+            {"Radix Sort", radixSort<int>},
+            {"Counting Sort", countingSort<int>}
+        };
 
-    vector<pair<string, function<void(vector<int>&)>>> algorithmsGroupThreeWithAlreadySortedVector = {
-        {"Radix Sort", radixSort<int>},
-        {"Counting Sort", countingSort<int>}
-    };
+        vector<pair<string, function<void(vector<int>&)>>> algorithmsGroupTwoWithReverseSortedVector = {
+            {"Heap Sort", heapSort<int>},
+            {"Merge Sort", mergeSort<int>},
+            {"Natural Merge Sort", naturalMergeSort<int>},
+            {"Quick Sort", quickSort<int>},
+            {"std::sort", stdSort<int>},
+            {"Shell Sort", shellSort<int>},
+            {"Radix Sort", radixSort<int>},
+            {"Counting Sort", countingSort<int>}
+        };
 
-    testSortingAlgorithms(algorithmsGroupOne, originalVectorGroupOne);
-    testSortingAlgorithms(algorithmsGroupTwo, originalVectorGroupTwoAndThree);
-    testSortingAlgorithms(algorithmsGroupThree, originalVectorGroupTwoAndThree);
+        vector<pair<string, function<void(vector<int>&)>>> algorithmsGroupOneWithNearlySortedVector = {
+            {"Selection Sort", selectionSort<int>},
+            {"Insertion Sort", insertionSort<int>},
+            {"Binary Insertion Sort", binaryInsertionSort<int>},
+            {"Bubble Sort", bubbleSort<int>},
+            {"Shaker Sort", shakerSort<int>},
+            {"Shell Sort", shellSort<int>},
+            {"Heap Sort", heapSort<int>},
+            {"Merge Sort", mergeSort<int>},
+            {"Natural Merge Sort", naturalMergeSort<int>},
+            {"Quick Sort", quickSort<int>},
+            {"std::sort", stdSort<int>},
+            {"Radix Sort", radixSort<int>},
+            {"Counting Sort", countingSort<int>}
+        };
 
-    testSortingAlgorithms(algorithmsGroupOneWithAlreadySortedVector, originalSortedVectorGroupOne);
-    testSortingAlgorithms(algorithmsGroupTwoWithAlreadySortedVecotr, originalSortedVectorGroupTwoAndThree);
-    testSortingAlgorithms(algorithmsGroupThreeWithAlreadySortedVector, originalSortedVectorGroupTwoAndThree);
+        vector<pair<string, function<void(vector<int>&)>>> algorithmsGroupTwoWithNearlySortedVector = {
+            {"Heap Sort", heapSort<int>},
+            {"Merge Sort", mergeSort<int>},
+            {"Natural Merge Sort", naturalMergeSort<int>},
+            {"Quick Sort", quickSort<int>},
+            {"std::sort", stdSort<int>},
+            {"Shell Sort", shellSort<int>},
+            {"Radix Sort", radixSort<int>},
+            {"Counting Sort", countingSort<int>}
+        };
+
+        cout << endl << "RANDOM VECTOR";
+        cout << endl << "\t\t Experiment 1 (n = " << sizeOfGroupOne << ", k = " << k << ")" << endl;
+        testSortingAlgorithms(algorithmsGroupOne, originalVectorGroupOne);
+        cout << endl << "\t\t Experiment 2 (n = " << sizeOfGroupTwoAndThree << ", k = " << k << ")" << endl;
+        testSortingAlgorithms(algorithmsGroupTwo, originalVectorGroupTwoAndThree);
+
+        cout << endl << "RANDOM ALREADY VECTOR";
+        cout << endl << "\t\t Experiment 1 (n = " << sizeOfGroupOne << ", k = " << k << ")" << endl;
+        testSortingAlgorithms(algorithmsGroupOneWithAlreadySortedVector, originalSortedVectorGroupOne);
+        cout << endl << "\t\t Experiment 2 (n = " << sizeOfGroupTwoAndThree << ", k = " << k << ")" << endl;
+        testSortingAlgorithms(algorithmsGroupTwoWithAlreadySortedVector, originalSortedVectorGroupTwoAndThree);
+
+        cout << endl << "RANDOM REVERSE SORTED VECTOR";
+        cout << endl << "\t\t Experiment 1 (n = " << sizeOfGroupOne << ", k = " << k << ")" << endl;
+        testSortingAlgorithms(algorithmsGroupOneWithReverseSortedVector, originalReverseSortedVectorGroupOne);
+        cout << endl << "\t\t Experiment 2 (n = " << sizeOfGroupTwoAndThree << ", k = " << k << ")" << endl;
+        testSortingAlgorithms(algorithmsGroupTwoWithReverseSortedVector, originalReverseSortedVectorGroupTwoAndThree);
+
+        cout << endl << "RANDOM NEARLY SORTED VECTOR";
+        cout << endl << "\t\t Experiment 1 (n = " << sizeOfGroupOne << ", k = " << k << ")" << endl;
+        testSortingAlgorithms(algorithmsGroupOneWithNearlySortedVector, originalNearlySortedVectorGroupOne);
+        cout << endl << "\t\t Experiment 2 (n = " << sizeOfGroupTwoAndThree << ", k = " << k << ")" << endl;
+        testSortingAlgorithms(algorithmsGroupTwoWithNearlySortedVector, originalNearlySortedVectorGroupTwoAndThree);
+    }
 
     return 0;
 }
